1d554034751c0c94813bae150d607ba0a577236d
Omar Cypha <omarcypha@gmail.com>
2022-08-17 20:32:51 +0000

    Edit: Making it resposive plus final finishing touches and icon

---
+++ main.js
@@ -0,0 +1,9 @@
+window.onload =  () => {
+    const hamb_menu = document.querySelector('.hamburger');
+    const mobile_menu = document.querySelector('.mobile-nav');
+
+    hamb_menu.addEventListener('click', () => {
+        hamb_menu.classList.toggle('is-active');
+        mobile_menu.classList.toggle('is-active');
+    });
+}
